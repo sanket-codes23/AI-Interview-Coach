6d582ff7e7cae17896e620ceb1f5d9bf3a64f227
Sanket Singhal <sanketsinghal23@gmail.com>
2026-07-02 19:52:20 +0530

    Fix CORS for Vercel

--- backend/src/app.js
+++ backend/src/app.js
@@ -10,9 +10,12 @@ const app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin: "http://localhost:5173",
-    credentials: true
-}))
+    origin: [
+        "http://localhost:5173",
+        "https://ai-interview-coach-nine-rho.vercel.app"
+    ],
+    credentials: true,
+}));
 
 app.use((req, res, next) => {
     console.log(req.method, req.url);
